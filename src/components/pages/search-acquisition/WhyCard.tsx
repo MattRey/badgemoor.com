31cfac07c4b68fd09389c764d5e1a897f8f6cba5
Matt Reynolds <mattreynolds@qogita.com>
2025-08-17 16:35:43 +0100

    fix: update transition on why use us cards

--- src/components/pages/search-acquisition/WhyCard.tsx
+++ src/components/pages/search-acquisition/WhyCard.tsx
@@ -15,11 +15,11 @@ const WhyCard: React.FC<WhyCardProps> = ({ title, image, text }) => {
       </div>
 
       <div className="absolute inset-0 flex flex-col">
-        <div className="bg-gray-400/60 p-4 group-hover:bg-[var(--primary)]">
+        <div className="bg-gray-400/60 p-4 group-hover:bg-[var(--primary)] transition-all duration-500 ease-in-out">
           <h3 className="text-white text-xl font-medium">{title}</h3>
         </div>
 
-        <div className="hidden group-hover:block bg-white/90 flex-1 p-4 overflow-y-auto no-scrollbar">
+        <div className="bg-white/90 flex-1 p-4 overflow-y-auto no-scrollbar transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 invisible group-hover:visible">
           <p className="text-gray-800">{text}</p>
         </div>
       </div>
